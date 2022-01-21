a52792e3d19f03751414f5d0cb3ae6c2bd44464c
Intelli <pun6sn@gmail.com>
2022-01-21 11:16:36 +0300

    Made code dry

--- Challenging/summ.py
+++ Challenging/summ.py
@@ -1,55 +1,39 @@
-import numpy as np
-import statistics
-import math
+# Lol no imports
 
-run = 1
-
-while run == 1:
+while True:
     print('1. Basic')
     print('2. Even')
     print('3. Odd')
 
-    type = int(input("Type Of Summ? >>>"))
+    inp = input("Type Of Summ/Exit? >>> ");
+    if(inp.lower() == "exit"):
+        break
 
-    if type == 1:
-        i = int(input("Start Value? >>>"))
-        k = int(input("Number Of Loops >>>"))
-        out2 = 0
+    i = int(input("Start Value? >>> "))
+    k = int(input("Number Of Loops >>> "))
+    type = int(inp)
+    out2 = 0
 
-        for i in range(i, k+1):
-            x = i
-            out = x
-            print(out)
-
-            out2 = out2 + out
-        print('Output =', out2)
-
-    if type == 2:
-        i = int(input("Start Value? >>>"))
-        k = int(input("Number Of Loops >>>"))
-        out2 = 0
-
-        for i in range(k):
-            x = i
-            out = 2 * x
-            print(out)
-
-            out2 = out2 + out
-        print(' ')
-        print('Output =', out2)
-        print(' ')
-
-    if type == 3:
-        i = int(input("Start Value? >>>"))
-        k = int(input("Number Of Loops >>>"))
-        out2 = 0
-
-        for i in range(k):
-            x = i
-            out = (2 * x) + 1
-            print(out)
-
-            out2 = out2 + out
-        print(' ')
-        print('Output =', out2)
-        print(' ')
+    match type:
+        case 1:
+            for i in range(i, k+1):
+                x = i
+                out = x
+                print(out) # These repeated statements could be cut down too but I'm too lazy
+                out2 = out2 + out
+        case 2:
+            for i in range(k):
+                x = i
+                out = 2 * x
+                print(out)
+                out2 = out2 + out
+        case 3:
+            for i in range(k):
+                x = i
+                out = (2 * x) + 1
+                print(out)
+                out2 = out2 + out
+    
+    print(' ')
+    print('Output =', out2)
+    print(' ')
