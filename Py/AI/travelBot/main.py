d2e4105ab9e8893a30a6ec0b7b85ea1feaf11788
Pekay <pekaytab@gmail.com>
2023-03-28 03:13:47 -0400

    Updates

---
+++ Py/AI/travelBot/main.py
@@ -0,0 +1,3 @@
+# Print intro
+print("TravelBot 1.0")
+print("Welcome to our Travel Planning chatbot! I'm here to help you plan your next vacation. Let's get started by learning a bit more about what you're looking for.")
